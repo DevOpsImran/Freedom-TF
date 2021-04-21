4e34cf7e962ffe9e165cd904ed9f4897b2548ecc
Harsha Vardhan Chamarthi <hvchamarthi@apptium.com>
2021-04-21 13:53:04 +0530

    IAM Commit

---
+++ IAM-Role.tf
@@ -0,0 +1,198 @@
+provider "aws"{
+  region  = var.aws_region
+  access_key=var.access_key
+  secret_key=var.secret_key
+
+}
+
+resource  "aws_iam_role" "role" {
+  name = "Freedom-nonprod-iam-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_policy" "policy-ssm" {
+  name        = "Freedom-nonprod-AmazonEC2RoleforSSM"
+  path        = "/"
+  description = "Role for SSM"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:PutMetricData"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstanceStatus"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ds:CreateComputer",
+                "ds:DescribeDirectories"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketLocation",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetEncryptionConfiguration",
+                "s3:AbortMultipartUpload",
+                "s3:ListMultipartUploadParts",
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+######################
+resource "aws_iam_policy" "policy-s3" {
+  name        = "Freedom-nonprod-S3-full-Access"
+  path        = "/"
+  description = "Role for S3 full access freedom"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+######################
+resource "aws_iam_policy" "policy-cloudwatch" {
+  name        = "Freedom-nonprod-cloudwatch-full-Access"
+  path        = "/"
+  description = "Role for cloudwatch full access freedom"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:Describe*",
+                "cloudwatch:*",
+                "logs:*",
+                "sns:*",
+                "iam:GetPolicy",
+                "iam:GetPolicyVersion",
+                "iam:GetRole"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*",
+            "Condition": {
+                "StringLike": {
+                    "iam:AWSServiceName": "events.amazonaws.com"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+#######################
+          
+resource "aws_iam_role_policy_attachment" "policy_attach-ssm" {
+  role = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy-ssm.arn
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach-s3" {
+  role = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy-s3.arn
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach-cloudwatch" {
+  role = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy-cloudwatch.arn
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ApptiumDemo-1
