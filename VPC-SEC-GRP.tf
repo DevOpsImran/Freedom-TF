8a659698fdb699a785dac6857321cb1a9ba768a2
root <root@ip-172-31-28-225.ca-central-1.compute.internal>
2021-04-21 11:07:29 +0000

    remote VPC-SEC-GRP.tf

--- VPC-SEC-GRP.tf
+++ VPC-SEC-GRP.tf
@@ -1,10 +1,8 @@
-provider "aws"{
-  region  = var.aws_region
-  access_key=var.access_key
-  secret_key=var.secret_key
-
-}
-
+#provider "aws" {
+#  region  = var.aws_region
+#  access_key=var.access_key
+#  secret_key=var.secret_key
+#}
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpcCIDRblock
   enable_dns_hostnames = true
